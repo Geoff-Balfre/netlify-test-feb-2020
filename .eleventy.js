f3ef4b706f84bf9bbf97413bc67fc3d110c75c60
Geoff-Balfre <geoff@balfre.com>
2020-02-29 01:02:41 +0000

    eleventy.js update passthrough icons

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
 module.exports = function(eleventyConfig) {
+
   eleventyConfig.addPassthroughCopy("./assets");
   eleventyConfig.addPassthroughCopy("./css");
   eleventyConfig.addPassthroughCopy("./js");
+
+  eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy("favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("favicon-32x32.png");
+
 };
